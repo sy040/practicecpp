b90461258388c6b576f9272ec60238feb6f13af5
sy040 <shreyaaa2004@gmail.com>
2026-07-11 02:03:43 +0530

    array11

---
+++ arraynvectors/maxsubarrayprdts.cpp
@@ -0,0 +1,24 @@
+// Maximum Product Subarray
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"Enter range: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter values: ";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int currm,maxm=1;
+    for(i=0;i<n;i++){
+        currm=1;
+        for(j=i;j<n;j++){
+            currm*=arr[j];
+            if(currm>maxm){
+                maxm=currm;
+            }
+        }
+    }
+    cout<<"The max product is: "<<maxm;
+}
